6e6dfdb7e4de8102b6f584a1ecd1af563b0ace70
unknown <do.potolka.korona@gmail.com>
2021-04-26 00:17:37 +0500

    fallback

--- pages/breeds/[slug].js
+++ pages/breeds/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful'
 import Image from 'next/image'
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton'
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -20,7 +21,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   }
 }
 
@@ -36,6 +37,8 @@ export const getStaticProps = async ({ params }) => {
 }
 
 export default function BreedsDetails({ breed }) {
+  if (!breed) return <Skeleton />
+
   const { featuredImage, title, timeLife, params, method } = breed.fields
 
   return (
